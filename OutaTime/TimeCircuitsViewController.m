502329fcbd09ad6fae7fa04e70914d60b0552a82
Jorge Catalan Jr <catalanjrj@gmail.com>
2016-04-13 16:29:09 -0400

    implemented some code tasks

--- OutaTime/TimeCircuitsViewController.m
+++ OutaTime/TimeCircuitsViewController.m
@@ -9,7 +9,7 @@
 #import "TimeCircuitsViewController.h"
 #import "DatePickerViewController.h"
 
-@interface TimeCircuitsViewController ()
+@interface TimeCircuitsViewController ()<TimeCircuitsDatePickerDelegate>
 {
     //
     // 1. We need three variables to hold various info.
@@ -18,9 +18,9 @@
     //    The last is an NSInteger object to hold the current speed of the DeLorean.
     //
     
-//    NSTimer
-//    NSDateFormatter
-//    NSInteger
+    NSTimer *speedTimer;
+    NSDateFormatter *dateFormatter;
+    NSInteger currentSpeed;
 }
 
 // These are the properties that will be wired up to the labels in the storyboard. If the circles to the left of them are hollow, they have not been connected in the storyboard.
@@ -31,7 +31,7 @@
 
 // This is an IBAction. It is a method that will fire when the element it's connected to fires an event of your choosing.
 - (IBAction)travelBack:(UIButton *)sender;
-- (IBAction)setDestinationTime:(UIButton *)sender;
+//- (IBAction)setDestinationTime:(UIButton *)sender;
 
 // These are private custom methods
 - (void)startTimer;
@@ -49,37 +49,39 @@
     //
     // 2. The view should be titled "Time Circuits"
     //
-    
+    self.title = @"Time Circuits";
     //
     // 3. This is a good place to initialize the objects that will be used later on.
     //    The date formatter object you created above need to be instantiated.
     //
-    
+    dateFormatter = [[NSDateFormatter alloc]init];
     //
     // 4. Once created, the formatString you see below needs to be set as the date formatter's dateFormat
     //
     NSString *formatString = [NSDateFormatter dateFormatFromTemplate:@"MMMddyyyy"
                                                              options:0
                                                               locale:[NSLocale currentLocale]];
+    [dateFormatter setDateFormat:formatString];
     
     //
     // 5. The presentTimeLabel needs to be set to today's date. Use the dateFormatter object to do this.
     //
-    
+    self.presentTimeLabel.text = [dateFormatter stringFromDate: [NSDate date]];
     
     //
     // 6. The currentSpeed integer object needs to be set to 0 to start.
     //
-
+    currentSpeed = 0;
     
     //
     // 7. The speedLabel should be set to "% MPH", with the % being the current speed
     //
-
+    self.speedLabel.text = [NSString stringWithFormat:@"%ld MPH", currentSpeed];
     
     //
     // 8. The lastTimeDeparted label needs to be set to "--- -- ----"
     //
+    self.lastTimeDepartedLabel.text = @"--- -- ----";
 
 }
 
@@ -100,7 +102,8 @@
         //
         // 10. This view controller needs to be set as the time picker view controller's delegate object.
         //
-        
+        //.delegat tells what to do
+        timePickerVC.delegate = self;
     }
 }
 
@@ -111,7 +114,7 @@
     //
     // 12. The destinationTimeLabel needs to be set to the destination date using our date formatter object
     //
-    
+    self.destinationTimeLabel.text = [dateFormatter stringFromDate:destinationDate];
 }
 
 #pragma mark - Action Handlers
